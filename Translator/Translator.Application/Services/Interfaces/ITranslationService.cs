4ec04561f0f1781b0c255443e9b8ccb1d50c57ce
Juanma <jmoliveras@kiteris.com>
2024-01-03 17:06:27 +0100

    Replace hardcoded string with constant

--- Translator/Translator.Application/Services/Interfaces/ITranslationService.cs
+++ Translator/Translator.Application/Services/Interfaces/ITranslationService.cs
@@ -1,8 +1,10 @@
-﻿namespace Translator.Application.Services.Interfaces
+﻿using Translator.Application.Constants;
+
+namespace Translator.Application.Services.Interfaces
 {
     public interface ITranslationService
     {
-        Task<string> TranslateText(string text, string language = "es");
+        Task<string> TranslateText(string text, string language = Languages.Spanish);
         Task<string> DetectLanguage(string text);
     }
 }
